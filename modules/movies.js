0663b579869dc46260c266ed00583efba8673c88
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2023-03-24 01:41:29 -0500

    backend composure

---
+++ modules/movies.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const axios = require('axios');
+
+async function getMovies(request, response, next){
+  try {
+    let searchQuery = request.query.query;
+
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}&language=en-US&page=1&include_adult=false`;
+
+    let movieResults = await axios.get(url);
+
+    let dataToSend = movieResults.data.results.map(movie => new Movies(movie));
+
+    response.status(200).send(dataToSend);
+  } catch (error) {
+    next(error);
+  }
+}
+
+class Movies {
+  constructor(movieObj){
+    this.title = movieObj.title;
+    this.overview = movieObj.overview;
+    this.average_votes = movieObj.vote_average;
+    this.total_votes = movieObj.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w500${movieObj.poster_path}`;
+    this.popularity = movieObj.popularity;
+    this.released_on = movieObj.release_date;
+  }
+}
+
+module.exports = getMovies;
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ console.log('helloo world');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const axios = require('axios');
-const { request, response } = require('express');
+const getWeather = require('./modules/weather.js');
+const getMovies = require('./modules/movies.js');
 
 // let data = require('./data/weather.json');
 
@@ -27,7 +27,7 @@ app.listen(PORT, () => console.log(`Running on port ${PORT}`));
 // ****** ENDPOINTS ****
 
 app.get('/', (request, response) => {
-  response.status(200).send('Welcome to me Server');
+  response.status(200).send('Welcome to my Server');
 });
 
 app.get('/hello', (request, response) => {
@@ -38,59 +38,12 @@ app.get('/hello', (request, response) => {
   response.status(200).send(`Hello ${userfirstName} ${userlastName}! Welcome to my Website`);
 });
 
-app.get('/weather', async(request, response, next) => {
-  try{
-    let lat = request.query.lat;
-    let lon = request.query.lon;
 
-    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=5&units=I`;
+app.get('/weather', getWeather);
 
-    let weatherResults = await axios.get(url);
 
-    // let dataToGroom = data.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
-    let dataToSend = weatherResults.data.data.map(day => new Forecast(day));
+app.get('/movies', getMovies);
 
-    response.status(200).send(dataToSend);
-  } catch(error){
-    next(error);
-  }
-});
-
-class Forecast {
-  constructor(cityObj){
-    this.date = cityObj.datetime;
-    this.description = cityObj.weather.description;
-  }
-}
-
-app.get('/movies', async(request, response, next) => {
-  try {
-    let searchQuery = request.query.query;
-
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}&language=en-US&page=1&include_adult=false`;
-
-    let movieResults = await axios.get(url);
-
-    let dataToSend = movieResults.data.results.map(movie => new Movies(movie));
-
-    response.status(200).send(dataToSend);
-  } catch (error) {
-    next(error);
-  }
-
-});
-
-class Movies {
-  constructor(movieObj){
-    this.title = movieObj.title;
-    this.overview = movieObj.overview;
-    this.average_votes = movieObj.vote_average;
-    this.total_votes = movieObj.vote_count;
-    this.image_url = `https://image.tmdb.org/t/p/w500${movieObj.poster_path}`;
-    this.popularity = movieObj.popularity;
-    this.released_on = movieObj.release_date;
-  }
-}
 
 // *** Catch All
 app.get('*', (request, response) => {
